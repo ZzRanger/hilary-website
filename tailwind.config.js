e55779696261fdfbc398829c1d9e428f2363ae5c
Jason Antwi-Appah <hey@jasonaa.me>
2023-04-09 20:31:59 -0500

    chore: initial set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  plugins: [],
   darkMode: 'class',
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        "hn-pink": "#F0AECE",
+        "hn-blue": "#BEE3E6",
+        "hn-yellow": "#FAD67F",
+        "hn-green": "#D1E1D6"
+    }
+  }
+}
 }
