553ecaf66f9f0ccc1062303eff4e36c55d28e02d
Jason Antwi-Appah <hey@jasonaa.me>
2023-04-14 01:06:12 -0500

    feat: impl pill selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+'use client';
+
+// TODO: refactor into separate components so we can use server components where possible
 import Image from 'next/image';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import { NavBar } from './components/NavBar';
 
 export default function Home() {
+  const options = ['UX/UI', 'Graphics'];
+  const [option, setOption] = useState(0);
   return (
-    <div>
+    <div className="flex flex-col items-center gap-6">
       <NavBar />
-      <div className="flex flex-col items-center text-6xl leading-">
+      <div className="flex flex-col items-center text-6xl">
         <h2>
           Hello, my name is{' '}
           <SpecialText className="text-8xl">Hilary</SpecialText>!
@@ -15,9 +20,40 @@ export default function Home() {
           I like to design <SpecialText>cool stuff</SpecialText>
         </h2>
       </div>
+      <PillSelector
+        options={options}
+        selected={option}
+        setSelected={setOption}
+      />
     </div>
   );
 }
+type PillSelectorProps = {
+  options: string[];
+  selected: number;
+  setSelected: (selected: PillSelectorProps['selected']) => void;
+};
+
+const PillSelector = ({
+  options,
+  selected,
+  setSelected,
+}: PillSelectorProps) => (
+  <div className="flex flex-row items-center justify-center rounded-full dark:bg-[#6b7280] w-max overflow-clip px-1">
+    {options.map((option, index) => (
+      <button
+        type="button"
+        key={`pill-option-${index}`}
+        onClick={() => setSelected(index)}
+        className={`my-1 rounded-full px-4 py-1 ${
+          selected === index ? 'dark:bg-[#323943]' : 'bg-inherit'
+        }`}
+      >
+        {option}
+      </button>
+    ))}
+  </div>
+);
 
 const SpecialText = ({
   children,
