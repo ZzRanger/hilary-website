9268d72d3571dedb6bdfa102b02ad68d8e0f9b67
ZzRanger <calebmarklim@gmail.com>
2025-01-03 22:07:00 -0600

    save

--- src/components/svg/DownloadIcon.tsx
+++ src/components/svg/DownloadIcon.tsx
@@ -13,9 +13,9 @@ const DownloadIcon: FC<
     <path
       d="M3 14.4231L3 15.2981C3 16.7478 4.17525 17.9231 5.625 17.9231L14.375 17.9231C15.8247 17.9231 17 16.7478 17 15.2981L17 14.4231M13.5 10.9231L10 14.4231M10 14.4231L6.5 10.9231M10 14.4231L10 3.9231"
       stroke="white"
-      stroke-width="1.67"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.67"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
